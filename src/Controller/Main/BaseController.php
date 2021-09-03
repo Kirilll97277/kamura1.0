e1c898af1ebd9a1f1f67d28ef909544c7a3443d7
kirill <Kirill19970806@gmail.com>
2021-09-03 15:31:35 +0300

    1.1

---
+++ src/Controller/Main/BaseController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\Main;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BaseController extends AbstractController
+{
+    public function renderDefault()
+    {
+        return [
+            'title' => 'Значение по умолчанию',
+        ];
+    }
+}
